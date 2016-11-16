a3ea5c74b1a69852cbbdf15b9a8d6fe3a25d2ba8
Oleg Grigoryev <oleg_grigoryev@epam.com>
2016-11-16 18:38:22 +0300

    open_addressing_hash_map_test

---
+++ test/collects/open_addressing_hash_map_test.py
@@ -0,0 +1,26 @@
+import unittest
+import collects.open_addressing_hash_map
+
+
+class HashMapTest (unittest.TestCase):
+    def test(self):
+        map = collects.open_addressing_hash_map.HashMap(10)
+        map.put(1, "one")
+        map.put(11, "another one")
+        map.put("two", "two")
+        map.put("two", "two again") # "two again" should replace "two"
+
+        self.assertEqual("one", map.get(1))
+        self.assertEqual("another one", map.get(11))
+        self.assertEqual("two again", map.get("two"))
+
+    def test_overflow(self):
+        map = collects.open_addressing_hash_map.HashMap(4)
+        map.put(7, "something")
+        map.put(17, "something")
+        map.put(27, "something")
+        map.put(37, "something")
+
+
+        self.assertRaises(Exception, map.put(47, "something"))
+
