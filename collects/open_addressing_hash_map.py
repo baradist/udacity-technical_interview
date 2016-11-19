461980140075b1bd24c21bccc09cd0a5ebb6db6b
Oleg Grigoryev <oleg_grigoryev@epam.com>
2016-11-19 17:22:46 +0300

    HashMap + put_all() + test

--- collects/open_addressing_hash_map.py
+++ collects/open_addressing_hash_map.py
@@ -7,7 +7,7 @@ class Node:
 class HashMap:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.l = [0 for i in range(0, capacity)]
+        self.l = [0 for i in range(0, capacity)] # todo
         self._size = 0
 
     def _hash(self, key):
@@ -20,6 +20,13 @@ class HashMap:
         if not self.l[index]:
             self._size += 1
         self.l[index] = Node(key, value)
+
+    def put_all(self, map):
+        if self._size + map.size() >= self.capacity:
+            raise Exception("Nowhere to put, map is full, size and capacity =", self.capacity, "(trying to add", map.size())
+        for i in range(0, map.capacity):
+            if map.l[i]:
+                self.put(map.l[i].key, map.l[i].value)
 
     def get(self, key):
         return self._get(key).value
--- test/collects/open_addressing_hash_map_test.py
+++ test/collects/open_addressing_hash_map_test.py
@@ -24,3 +24,15 @@ class HashMapTest (unittest.TestCase):
         with self.assertRaises(Exception):
             map.put(47, "something")
 
+    def testPutAll(self):
+        map = collects.open_addressing_hash_map.HashMap(10)
+        new_map = collects.open_addressing_hash_map.HashMap(10)
+        map.put(1, "one")
+        map.put(11, "another one")
+        map.put("two", "two")
+        map.put("two", "two again")  # "two again" should replace "two"
+        new_map.put_all(map)
+
+        self.assertEqual("one", new_map.get(1))
+        self.assertEqual("another one", new_map.get(11))
+        self.assertEqual("two again", new_map.get("two"))
